f734f85cd73bddf8e5bb8a5fbfc0d9b05dfea5ee
Dinh Tuan Le <letuan@fit.cvut.cz>
2020-01-08 12:28:14 +0100

    Retouched club views

--- semestralka/src/Form/ClubType.php
+++ semestralka/src/Form/ClubType.php
@@ -49,7 +49,6 @@ class ClubType extends AbstractType
                 'class' => Account::class, 'choice_label' => 'username',
                 'multiple' => true, 'required' => false, 'label' => 'Members: '
             ])
-            ->add( 'submit', SubmitType::class, ['label' => 'Create'])
         ;
     }
 
